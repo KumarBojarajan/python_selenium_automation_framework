ab6b8ab9f0b591d98e40fa080de827ccb5d50531
Mani kumar <manikumar.bojarajan@genesys.com>
2022-12-15 20:07:50 +0530

    added comments for page chaining model

--- Tests/test_home.py
+++ Tests/test_home.py
@@ -1,9 +1,11 @@
 from Tests.test_base import BaseTest
 from Pages.login_page import LoginPage
+from Pages.home_page import HomePage
 from Config.config import TestData
 class Test_Home(BaseTest):
     
     def test_logout(self):
         self.loginpage = LoginPage(self.driver)
-        self.home_page_instance = self.loginpage.do_login(TestData.USER_NAME, TestData.PASSWORD)      
+        # login page returns landing page object ( i.e homepage) - this concept is called page chaining
+        self.home_page_instance = self.loginpage.do_login(TestData.USER_NAME, TestData.PASSWORD)     
         self.home_page_instance.logout()
