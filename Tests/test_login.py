621cf355ddeb4f53405f813779e37938daafa6d7
Mani kumar <manikumar.bojarajan@genesys.com>
2022-12-15 20:08:23 +0530

    sample code for 'test skip' and 'screenshot'

--- Tests/test_login.py
+++ Tests/test_login.py
@@ -1,7 +1,17 @@
 from Tests.test_base import BaseTest
 from Pages.login_page import LoginPage
 from Config.config import TestData
+import pytest
+import time
 class Test_login(BaseTest):
+    @pytest.mark.skip()
     def test_login(self):
         self.loginpage = LoginPage(self.driver)
         self.loginpage.do_login(TestData.USER_NAME, TestData.PASSWORD)
+    
+    def test_right_click(self, request):
+        try:
+            self.loginpage = LoginPage(self.driver)
+            self.loginpage.right_click_forgot_password_link()
+        except:
+            self.loginpage.take_screenshot(request.node.name)
